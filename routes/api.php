6936af5acc2f99f0b822becc95574be32821defb
abimon <eddyabby59@gmail.com>
2024-07-10 16:41:58 +0300

    update auth user

--- routes/api.php
+++ routes/api.php
@@ -11,11 +11,11 @@ Route::get('/user', function (Request $request) {
 Route::controller(UserController::class)->prefix('/user')->group(function(){
         Route::post('/Login','index');
         Route::post('/Register','store');
-        Route::put('/update/{id}','update')->middleware('auth:sanctum');
+        Route::put('/update','update')->middleware('auth:sanctum');
     Route::get('/get', 'show')->middleware('auth:sanctum');
 });
 Route::controller(AppartmentsController::class)->prefix('/property')->group(function () {
     Route::get('/index','index');
     Route::post('/store','store')->middleware('auth:sanctum');
-    Route::put('/update','update')->middleware('auth:sanctum'); 
+    Route::put('/update/{id}','update')->middleware('auth:sanctum'); 
 });
